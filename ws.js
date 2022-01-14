6417ed21f382ed5f8c5b7d513f2dbe1bdc4cba22
Niaz Hussain <niazhussain1990@gmail.com>
2022-01-14 18:32:20 +0100

    default port updated

--- ws.js
+++ ws.js
@@ -1,5 +1,5 @@
 var WebSocketServer = require('ws').Server,
-  wss = new WebSocketServer({port: 40510})
+  wss = new WebSocketServer({port: 3000})
 
 wss.on('connection', function (ws) {
   ws.on('message', function (message) {
